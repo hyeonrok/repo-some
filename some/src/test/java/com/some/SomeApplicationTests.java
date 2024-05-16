2e3f22888a412cae12aaf0d7fc346e6c1a4c9e7e
hyeonrok <wjsgusfhr@naver.com>
2024-05-16 17:40:14 +0900

    a

---
+++ some/src/test/java/com/some/SomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.some;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
